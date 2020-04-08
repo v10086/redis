f26146390bc157d7b049b55099b75cab69398d69
zhongbo <1146625863@qq.com>
2020-04-09 00:36:21 +0800

    redis操作句柄

---
+++ src/Redis.php
@@ -0,0 +1,54 @@
+<?php
+namespace v10086;
+
+//Redis 操作句柄  要求安装phpredis扩展
+class Redis {
+    
+    static $instance;//实例组
+    static $handler;//当前操作句柄实例
+    static $cofing;//配置信息
+    
+    //初始化redis客服端并连接服务器端
+    public static function connection($conn='default'){
+        $conn && self::$handler=$conn;  !self::$handler && self::$handler ='default';
+        
+        if (!isset(self::$instance[self::$handler]) || !self::$instance[self::$handler]){
+            if(!self::$cofing){
+                throw new \Exception('请先设置配置信息');
+            }
+            $cofing=self::$cofing[self::$handler];
+            if(!isset(self::$instance[self::$handler]) || empty(self::$instance[self::$handler]) ){
+                self::$instance[self::$handler] =new \Redis();
+                $func = $cofing['persistent'] ? 'pconnect' : 'connect';
+                $cofing['timeout'] === false ?
+                self::$instance[self::$handler]->$func($cofing['host'], $cofing['port']) :
+                self::$instance[self::$handler]->$func($cofing['host'], $cofing['port'], $cofing['timeout']);
+                $cofing['password'] &&  self::$instance[self::$handler]->auth($cofing['password']);
+            }
+        }
+        
+        return self::$instance[$handler];
+    }
+    
+    
+//     @desc 以static 方式编码 执行Redis方法 例如 Redis::pop();
+//     @param method 需要执行的方法
+//     @param arguments method所需参数
+//     @return redis执行结果
+    public static function __callStatic($method, $arguments) {
+        try{
+            if (!isset(self::$instance[self::$handler]) || !self::$instance[self::$handler]) self::connection(self::$handler);
+            return call_user_func_array([self::$instance[self::$handler], $method], $arguments); 
+        } catch (\RedisException $e) {
+            if ($e->getMessage() == 'Redis server went away' || $e->getMessage() == 'Invalid connect timeout') {
+                self::$instance[self::$handler]=null;
+                self::connection(self::$handler);
+                return call_user_func_array([self::$instance[self::$handler], $method], $arguments); 
+            }else{
+                throw new \Exception($e->getMessage());
+            }
+        }
+        
+    }
+
+}
